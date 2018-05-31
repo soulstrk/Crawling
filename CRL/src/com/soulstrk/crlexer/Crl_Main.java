a5b74d264a2358af2031ea633fe9a9ba4d56932b
재용 <@218.145.210.169>
2018-05-31 22:54:08 +0900

    test 5-31

--- CRL/src/com/soulstrk/crlexer/Crl_Main.java
+++ CRL/src/com/soulstrk/crlexer/Crl_Main.java
@@ -1,25 +1,29 @@
 package com.soulstrk.crlexer;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-import org.jsoup.*;
-import org.jsoup.nodes.*;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public class Crl_Main {
 
 	public static void main(String[] args) {
+		
 		try {
-			Document doc = Jsoup.connect("https://www.tiobe.com/tiobe-index/").get();
+			Document doc = Jsoup.connect("https://www.melon.com/chart/index.htm").get();
 
-			Element rank = doc.select(".table tbody").get(0);
-			Elements ranks = rank.select("tr"); 
+			Elements songTitle = doc.select(".lst50").select("td:eq(5)").select(".rank01");
+			Elements rank = doc.select(".lst50").select("td:eq(2)");
 
-			for (Element e : ranks) {
-				System.out.println(e.text());
-			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 }
+
+
